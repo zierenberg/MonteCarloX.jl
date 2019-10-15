6672990852cd74115ffbb35b1ac3e81bf8295575
Johannes Zierenberg <you@example.com>
2019-10-15 09:58:04 +0200

    first version with Metropolis update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using MonteCarloX
 using Test
+using Random
 
 @testset "MonteCarloX.jl" begin
-    # Write your own tests here.
+  # Write your own tests here.
+  @test Metropolis.update(1,2,1,MersenneTwister(1000))==true
 end
