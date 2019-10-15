0ab93a4b3b874488d9980472ff67b54cb065e183
Johannes <johannes.zierenberg@gmail.com>
2019-10-15 14:44:12 +0200

    added non-equilibrium Gillespie update and example implementation with a rough test (close to analytic result, but needs further testing)

--- src/MonteCarloX.jl
+++ src/MonteCarloX.jl
@@ -5,8 +5,7 @@ greet() = print("Hello World!")
 #todo: sort code according to classic, canonical, ..? 
 
 include("Metropolis.jl")
-#include("gillespie.jl")
+include("Gillespie.jl")
 
-testf() = 5
 
 end # module
