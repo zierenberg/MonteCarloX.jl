63c64902730f60bd07773ba82b6d0473b0561618
fabi <fmikulasch@googlemail.com>
2019-10-15 14:20:27 +0200

    implemented cluster update; no testing done

--- src/MonteCarloX.jl
+++ src/MonteCarloX.jl
@@ -5,7 +5,7 @@ greet() = print("Hello World!")
 #todo: sort code according to classic, canonical, ..? 
 
 include("Metropolis.jl")
-#include("gillespie.jl")
+include("ClusterWolff.jl")
 
 
 end # module
