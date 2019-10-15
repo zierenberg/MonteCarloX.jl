dad4371b379aa6a7c669748ece1e731b3f5adef1
fabi <fmikulasch@googlemail.com>
2019-10-15 18:51:11 +0200

    fixed update

--- src/ClusterWolff.jl
+++ src/ClusterWolff.jl
@@ -2,9 +2,7 @@ module ClusterWolff
 using Random
 using LightGraphs
 
-#abstract type Ising
-
-struct Ising
+struct IsingSystem
   dims :: Array{Int32}
   lattice :: SimpleGraph
   spins :: Array{Int32}
@@ -13,7 +11,32 @@ end
 function constructIsing(dims, rng)
   lattice = LightGraphs.SimpleGraphs.grid(dims, periodic=true)
   spins = rand(rng, [-1,1], dims...)
-  s = Ising(dims, lattice, spins)
+  s = IsingSystem(dims, lattice, spins)
+  return s
+end
+
+function magnetization(system::IsingSystem)
+  return abs(sum(system.spins))
+end
+
+function testRun(beta)
+
+  function m(N, beta, nSteps)
+    rng = MersenneTwister(1000)
+    system = constructIsing([N,N], rng)
+    
+    meanMagnetization = 0.0
+    for i = 1:nSteps
+      update(system, beta, rng)
+      #println(system.spins)
+      meanMagnetization += magnetization(system)
+    end
+    meanMagnetization /= nSteps
+
+    return meanMagnetization
+  end
+
+  return [m(10, beta, 50000) for beta in 0.0:0.05:2.0]
 end
 
 """
@@ -31,7 +54,7 @@ https://stanford.edu/~kurinsky/ClassWork/Physics271_Final_Paper.pdf
 
 consider BlumeCapel implementation
 """
-function update(system::Ising, beta, rng)
+function update(system::IsingSystem, beta, rng)
   N = length(system.spins)
 
   # Get random spin and flip it
@@ -44,12 +67,12 @@ function update(system::Ising, beta, rng)
   # rand(rng) < 1.0 - exp(-2*beta)
   p = 1.0 - exp(-2*beta)
   visited = BitSet([i])  
-  unvisited = outneighbors(system.lattice, i)
+  unvisited = copy(outneighbors(system.lattice, i))
   while !isempty(unvisited)
     j = pop!(unvisited)
     if !in(j, visited) && system.spins[j] == s_i && rand(rng) < p
       system.spins[j] *= -1
-      append!(outneighbors(system.lattice, j), unvisited)
+      append!(unvisited, outneighbors(system.lattice, j))
     end
     push!(visited, j)
   end
