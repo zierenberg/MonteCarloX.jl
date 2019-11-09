a687d531934b90cb3fa04ba3328b5812ceff7655
Johannes <johannes.zierenberg@gmail.com>
2019-11-09 23:22:07 +0100

    moved test from examples to test

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ import Distributions.cdf
   # Write your own tests here.
   @test Metropolis.update(x->exp(-1*x),1,2,MersenneTwister(1000))==true
 
-  include("../examples/inhomogeneous_poisson_tests.jl")
+  include("test_inhomogenous_poisson.jl")
   @test test_poisson_single()
   @test test_poisson_constant()
   @test test_poisson_sin_wave()
