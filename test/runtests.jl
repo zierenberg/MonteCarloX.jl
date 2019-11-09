d66731b2631269d1292b2621893d401613e22b8c
fabi <fmikulasch@googlemail.com>
2019-11-09 01:56:09 +0100

    additional test although i haven't tested it

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,7 @@ import Distributions.cdf
   @test Metropolis.update(x->exp(-1*x),1,2,MersenneTwister(1000))==true
 
   include("../examples/inhomogeneous_poisson_tests.jl")
+  @test test_poisson_single()
   @test test_poisson_constant()
   @test test_poisson_sin_wave()
 end
