ddcbac61cfa1a132987c1541c3683c39d774ca17
Johannes <johannes.zierenberg@gmail.com>
2020-01-23 22:32:02 +0100

    adeed documentation for binary_search

--- test/test_gillespie.jl
+++ test/test_gillespie.jl
@@ -4,14 +4,16 @@ using Random
 include("../examples/contact_process.jl")
 
 """ 
+    test_gillespie_fast(;verbose=false) 
+
 Testing if sampling single event correctly
 
-there is clearly a drift in the precision of sum_rates
+there is clearly a drift in the precision of sum_rates (needs to be compensated in system implementation of course
 """
 function test_gillespie_fast(;verbose=false)
   pass = true
-  fst_a, fst_a2 = run(Int(1e3),1.0,1e-3,1e-2,1e3,1e1,1000, flag_fast=true)
-  std_a, std_a2 = run(Int(1e3),1.0,1e-3,1e-2,1e3,1e1,1000, flag_fast=false)
+  fst_a, fst_a2 = run(Int(1e2),1.0,1e-3,1e-2,1e5,1e1,1000, flag_fast=true)
+  std_a, std_a2 = run(Int(1e2),1.0,1e-3,1e-2,1e5,1e1,1000, flag_fast=false)
   
   if verbose
     println("result average activity fast ($(fst_a)) and slow ($(std_a))")
