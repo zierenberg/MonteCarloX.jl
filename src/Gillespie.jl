caa3c3ae7daa40e159eb3e3bb42a0394aba1ba4d
Johannes <johannes.zierenberg@gmail.com>
2020-02-18 22:35:04 +0100

    EventHandler is own module with more and more consistent interface; Need to think about which eventhandlers should be kept

--- src/Gillespie.jl
+++ src/Gillespie.jl
@@ -8,7 +8,6 @@ module Gillespie
 using Random
 using Distributions
 using DataStructures
-using ..KineticMonteCarlo
 
 #TODO: ditch at some point
 #abstract type AbstractSystem end
@@ -17,6 +16,7 @@ using ..KineticMonteCarlo
 #function sum_rates(system::AbstractSystem) end
 
 using ..MonteCarloX
+using ..KineticMonteCarlo
 using ..EventHandler
 
 
@@ -27,17 +27,8 @@ Draw as many events for a `system` with a list of reactions with different
 rates such that the time of the last event is larger than `total_time` and
 return time of last event.
 """
-#function advance!(system::AbstractSystem, total_time::T, rng::AbstractRNG)::T where {T<:AbstractFloat}
-#  time::T = 0
-#  while time <= total_time
-#    dt, id = KineticMonteCarlo.next_event(list_rates(system), sum_rates(system), rng)
-#    time += dt
-#    update!(system,id)
-#  end
-#  return time 
-#end
 
-function advance!(event_handler::AbstractEventHandler, update!::Function, total_time::T, rng::AbstractRNG)::T where {T<:AbstractFloat}
+function advance!(event_handler::AbstractEventHandlerRate, update!::Function, total_time::T, rng::AbstractRNG)::T where {T<:AbstractFloat}
   time::T = 0
   while time <= total_time
     if num_events(event_handler) == 0
@@ -50,45 +41,6 @@ function advance!(event_handler::AbstractEventHandler, update!::Function, total_
   end
   return time 
 end
-
-
-###############################################################################
-###############################################################################
-###############################################################################
-#mutable struct EventQueue{T}<:AbstractEventHandler
-#  pq_event_rate::PriorityQueue{T,Float64} 
-#  noevent::T
-#  function EventQueue{T}(list_event::Vector{T}, list_time::Vector{Float64}, noevent::T) where T
-#    @assert length(list_event)==length(list_time)
-#    pq = PriorityQueue{T,Float64}()
-#    for i=1:length(list_event)
-#      enqueue!(pq,list_event[i]=>list_time[i])
-#    end
-#    new(pq, noevent)
-#  end
-#end
-#
-#function num_events(event_handler::EventQueue{T}) where T
-#  return length(event_handler.pq_event_rate)
-#end
-#
-#function set!(event_handler::EventQueue, event::T, time::Float64) where T
-#  event_handler.pq_event_rate[event] = time 
-#end
-#
-#function advance!(pq::PriorityQueue{T}, update!::Function, total_time::T, rng::AbstractRNG)::T where {T<:AbstractFloat}
-#  time::T = 0
-#  while time <= total_time
-#    if lengthj(event_handler) == 0
-#      println("WARNING: no events left before total_time reached")
-#      return time
-#    end
-#    event,time = dequeue_pair!(event_handler.pq_event_rate) 
-#    update!(event_handler, event)
-#  end
-#  return time 
-#end
-
 
 end
 export Gillespie
