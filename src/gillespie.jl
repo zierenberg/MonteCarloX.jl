78793b4157a9c3af8c7a754a724fc1dc713d1f35
Johannes Zierenberg <johannes.zierenberg@gmail.com>
2022-04-26 15:16:17 +0200

    fixed broken test after restructuring

--- src/gillespie.jl
+++ src/gillespie.jl
@@ -1,55 +1,31 @@
 # Gillespie simulations wrapper
-"""
-    Gillespie()
-
-Spezializations for Gillespie-type simulations derived from KineticMonteCarlo().
-
-# Basic functions
-* [`next`](@ref)
-* [`advance!`](@ref)
-
-# Additional functions
-* [`initialize`](@ref)
-"""
 struct Gillespie end
 
 """
-    initialize(alg::Gillespie, rates)
+    init(rng, Gillespie(), rates)
 
-creates `event handler` from a list of `rates` with most simple types of events
-[1:length(rates)]. With `type_event_handler` one can specify the class of event
-handler (default is `ListEventRateSimple`)
+create a simple KineticMonteCarlo simulation object that handles Gillespie-type
+simulation.
 
-#See also direct construction of event handlers
-[`ListEventRateSimple`](@ref), [`ListEventRateActiveMask`](@ref)
+[`SimulationKineticMonteCarlo`](@ref)
 """
-function initialize(alg::Gillespie,
-                    rates::Vector{Float64};
-                    type_event_handler::String = "ListEventRateSimple"
+function init(
+        rng::AbstractRNG,
+        alg::Gillespie,
+        rates::Vector{Float64},
+        type_event_handler::String = "ListEventRateSimple"
     )
-  events = collect(1:length(rates))
-  noevent = 0
-  threshold = 0.0
-  if type_event_handler == "ListEventRateSimple"
-   event_handler = ListEventRateSimple{Int}(events, rates, threshold, noevent)
-  elseif type_event_handler == "ListEventRateActiveMask"
-    event_handler = ListEventRateActiveMask{Int}(events, rates, threshold, noevent)
-  else
-    throw(UndefVarError(:type_event_handler))
-  end
+    events = collect(1:length(rates))
+    noevent = 0
+    threshold = 0.0
+    if type_event_handler == "ListEventRateSimple"
+     event_handler = ListEventRateSimple{Int}(events, rates, threshold, noevent)
+    elseif type_event_handler == "ListEventRateActiveMask"
+      event_handler = ListEventRateActiveMask{Int}(events, rates, threshold, noevent)
+    else
+      throw(UndefVarError(:type_event_handler))
+    end
 
-  return event_handler
+    return SimulationKineticMonteCarlo(rng, event_handler)
 end
-
-function next(rng::AbstractRNG, alg::Gillespie, event_handler::AbstractEventHandlerRate)::Tuple{Float64,Int}
-  next(rng, KineticMonteCarlo(), event_handler)
-end
-
-#should work already
-#function advance!(rng::AbstractRNG, alg::Gillespie, event_handler::AbstractEventHandlerRate, update!::Function, total_time::T)::T where {T <: AbstractFloat}
-#  advance!(rng, KineticMonteCarlo(), event_handler, update!, total_time)
-#end
-## If no rng is specified, uses Random.GLOBAL_RNG per default
-#function advance!(alg::Gillespie, event_handler::AbstractEventHandlerRate, update!::Function, total_time::T)::T where {T <: AbstractFloat}
-#  advance!(alg, event_handler, update!, total_time)
-#end
+init(alg::Gillespie, rates, type_event_handler) = init(Random.GLOBAL_RNG, alg, rates, type_event_handler)
